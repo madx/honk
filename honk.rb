773f505ecfc52eb5e105442e7984f77b56892a5b
madx <root@yapok.org>
2009-02-19 11:11:37 +0100

    adding the basics (post listing and view)

--- honk.rb
+++ honk.rb
@@ -1,28 +1,56 @@
 %w[rubygems sinatra pathname].each do |lib|
   require lib
 end
-class Pathname; alias / join; end
-require Pathname.new(__FILE__).dirname / 'lib' / 'honk'
+require File.join(File.dirname(__FILE__), 'lib', 'honk') 
 include Honk
 
 # configuration
 Honk.setup do
 end
 
+configure do
+  set :haml, :attr_wrapper => '"'
+end
+
+YAML.load_file(Honk.root / 'index.yml')
+
 helpers do
-  def partial(name, opts)
+  def partial(name, opts={})
     haml name, opts.merge(:layout => false)
+  end
+
+  def tag_link(t)
+    partial "%a{:href => '/tag/#{t}', :title => 'View posts tagged #{t}'} #{t}"
   end
 end
 
 get '/' do
+  page_num = params[:page].to_i || 0
+  begin
+    @posts = Index.page page_num
+  rescue Honk::OutOfRangeError
+    raise Sinatra::NotFound
+  end
+  haml :index 
 end
 
 get '/post/:name' do
+  if Index.has?(params[:name])
+    begin
+      @post = Post.open params[:name], Index.resolve(params[:name])
+    rescue Errno::ENOENT
+      raise Sinatra::NotFound
+    end
+    haml :post
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 get '/feed' do
 end
 
-get '/tag/:tag' do
+get '/_reload' do
+  YAML.load_file(Honk.root / 'index.yml')
+  "reloaded."
 end
