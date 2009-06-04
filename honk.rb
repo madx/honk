7eb23015f9e881fa7ffcc2f3a03d2a3af6f78694
madx <root@yapok.org>
2009-06-04 17:13:49 +0200

    refactored most of the helpers

--- honk.rb
+++ honk.rb
@@ -25,29 +25,117 @@ configure do
   set :haml, :attr_wrapper => '"'
 end
 
+TEMPLATES = {
+  :stylesheet => '%%link{%s}',
+  :link       => '%%a{%s} %s',
+  :tag_link   => '%%a{:href => "/tag/%s", :title => "View posts tagged %s"} %s',
+  :post_link  => '%%a{:href => "/post/%s", :title => "View this post"} %s',
+  :label      => '%%label{:for => "%s"} %s:',
+  :input      => '%%input{%s}',
+  :textarea   => '%%textarea{%s} %s',
+  :req_field  => '%%span.error This field is required'
+}
+
 # Helpers
 helpers do
   def partial(name, opts={})
     haml name, opts.merge(:layout => false)
   end
 
+  def template(name, *args)
+    partial TEMPLATES[name] % args
+  end
+
   def tag_link(t)
-    partial '%%a{:href => "/tag/%s", :title => "View posts tagged %s"} %s' %
-      [t, t, t]
+    template :tag_link, t, t, t
   end
 
   def post_link(p)
-    partial '%%a{:href => "/post/%s", :title => "View this post"} %s' %
-      [p.slug, p.title]
+    template :post_link, p.slug, p.title
+  end
+
+  def comments_link(post)
+    comment_string = "#{post.comments.length} comment"
+    comment_string << 's' if post.comments.length != 1
+    args = {
+      :href => "/post/#{post.slug}#comments",
+      :title => 'View comments for this post'
+    }
+    template :link, args.inspect, comment_string
+  end
+
+  def author_mailto_link
+    email = Honk.meta[:email].gsub('@', '[REMOVETHIS]@')
+    args = {
+      :href => "mailto:#{email}", :title => "Send a mail to the author"
+    }
+    template :link, args.inspect, email
+  end
+
+  def label_tag(name, caption)
+    template :label, name, caption
+  end
+
+  def input_field(name, caption)
+    value = params[name.to_sym] || request.cookies[name[2..-1]] || ''
+    member = @errors && @errors.member?(name.to_sym)
+
+    args = {
+      :type => "text", :name => name, :id => name,
+      :class => member ? "error" : "", :value => value
+    }
+
+    [
+      label_tag(name, caption),
+      template(:input, args.inspect),
+      field_required(member)
+    ].join
+  end
+
+  def text_field(name, caption)
+    contents = params[name.to_sym] || ''
+    member = @errors && @errors.member?(name.to_sym)
+
+    args = {
+      :name => name, :id => name, :rows => 15, :cols => 50,
+      :class => member ? "error" : ""
+    }
+
+    [
+      label_tag(name, caption),
+      template(:textarea, args.inspect, contents),
+      field_required(member)
+    ].join
+  end
+
+  def remember_check_box
+    args = {
+      :name => "c_remember", :id => "c_remember",
+      :type => "checkbox"
+    }
+    if request.cookies["remember"] then args["checked"] = "checked" end
+    template :input, args.inspect
+  end
+
+  def stylesheet(name)
+    args = {
+      :rel => "stylesheet", :type => "text/css",
+      :media => "screen", :href => versioned_css("/css/#{name}.css")
+    }
+    template :stylesheet, args.inspect
+  end
+
+  def versioned_css(file)
+    mtime = File.mtime(File.join(File.dirname(__FILE__), file)).to_i
+    "%s?%s" % [file, mtime]
+  end
+
+  def field_required(member)
+    member ? template(:req_field) : ''
   end
 
   def tag_url(t)
     "http://" + File.join(Honk.meta[:domain], 'tag', t)
-  end
-
-  def tag_item_count(items)
-    len = items.length
-    len == 1 ? "#{len} item" : "#{len} items"
   end
 
   def post_url(p)
@@ -58,84 +146,9 @@ helpers do
     "http://" + Honk.meta[:domain] + '/'
   end
 
-  def stylesheet(*names)
-    out = ''
-    names.each do |name|
-      args = {
-        :rel => "stylesheet", :type => "text/css",
-        :media => "screen", :href => versioned_css("/css/#{name}.css")
-      }
-      out << partial("%%link{%s}" % args.inspect)
-    end
-    out
-  end
-
-  def versioned_css(file)
-    "%s?%s" % [file, File.mtime(File.join(File.dirname(__FILE__), file)).to_i]
-  end
-
-  def comments_link(post)
-    comment_string = "#{post.comments.length} comment"
-    comment_string << 's' if post.comments.length != 1
-    params = {
-      :href => "/post/#{post.slug}#comments",
-      :title => 'View comments for this post'
-    }
-    partial '%%a{%s} %s' % [params.inspect, comment_string]
-  end
-
-  def author_mailto_link
-    email = Honk.meta[:email].gsub('@', '[REMOVETHIS]@')
-    args = {
-      :href => "mailto:#{email}", :title => "Send a mail to the author"
-    }
-    partial "%%a{%s} %s" % [args.inspect, email]
-  end
-
-  def label_tag(name, caption)
-    '%%label{:for => "%s"} %s: ' % [name, caption]
-  end
-
-  # Add error span if no member
-  def field_required(member, template)
-    member ? "\n" + '%span.error This field is required' : ''
-  end
-
-  def input_field(name, caption)
-    value = params[name.to_sym] || request.cookies[name[2..-1]] || ''
-    template = label_tag(name, caption) + "\n"
-    member = @errors && @errors.member?(name.to_sym)
-    args = {
-      :type => "text", :name => name, :id => name,
-      :class => member ? "error" : "", :value => value
-    }
-    template << '%%input{%s}' % args.inspect
-    template << field_required(member, template)
-    partial template
-  end
-
-
-  def text_field(name, caption)
-    contents = params[name.to_sym] || ''
-    template = label_tag(name, caption) + "\n"
-    member = @errors && @errors.member?(name.to_sym)
-    args = {
-      :name => name, :id => name, :rows => 15, :cols => 50,
-      :class => member ? "error" : ""
-    }
-    template << '%%textarea{%s} %s' % [args.inspect, contents]
-    template << field_required(member, template)
-    partial template
-  end
-
-  def remember_check_box
-    args = {
-      :name => "c_remember", :id => "c_remember",
-      :type => "checkbox"
-    }
-    if request.cookies["remember"] then args["checked"] = "checked" end
-    template = "%%input{%s}" % args.inspect
-    partial template
+  def tag_item_count(items)
+    len = items.length
+    len == 1 ? "#{len} item" : "#{len} items"
   end
 end
 
